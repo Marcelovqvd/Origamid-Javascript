a3c138b433000b14b07021e189a0543f6a049185
Marcelo <marcelovqvd@gmail.com>
2023-09-25 15:07:16 -0300

    modal

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initAccordion from "./modules/initAccordion.js"
 import initAnimacaoScroll from "./modules/initAnimacaoScroll.js"
 import initScrollSuave from "./modules/initScrollSuave.js"
 import initTabNav from "./modules/initTabNav.js"
+import initModal from "./modules/modal.js"
 
 initAnimacaoScroll()
 initScrollSuave()
 initAccordion()
 initTabNav()
+initModal()
