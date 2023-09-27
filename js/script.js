2807aff5392792bed65698dfe67bdaef6fa2a83c
Marcelo <marcelovqvd@gmail.com>
2023-09-27 11:08:34 -0300

    tooltip

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAnimacaoScroll from "./modules/initAnimacaoScroll.js"
 import initScrollSuave from "./modules/initScrollSuave.js"
 import initTabNav from "./modules/initTabNav.js"
 import initModal from "./modules/modal.js"
+import initTooltip from "./modules/tooltip.js"
 
 initAnimacaoScroll()
 initScrollSuave()
 initAccordion()
 initTabNav()
 initModal()
+initTooltip()
